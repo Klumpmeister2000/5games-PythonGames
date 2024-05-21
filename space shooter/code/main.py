2ffa946bdf8a31a28da55c4a552c40299e05625e
Klumpmeister2000 <christian@klumpmeister.net>
2024-05-21 16:22:35 -0400

    imported player_surf img and drew it to move.

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -7,10 +7,14 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
 
-# surface
+# plain surface
 surf = pygame.Surface((100,200))
 surf.fill('orange')
 x = 100
+
+#importing an image 
+
+player_surf = pygame.image.load('../images/player.png')
 
 while running:
     # event loop
@@ -21,7 +25,7 @@ while running:
     # draw the game
     display_surface.fill('darkgrey')
     x += 0.1
-    display_surface.blit(surf, (x, 150))
+    display_surface.blit(player_surf, (x, 150))
     pygame.display.update()
 
 
